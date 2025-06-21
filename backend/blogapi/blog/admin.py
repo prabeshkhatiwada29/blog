2c513153909db743358a605c6361aca3778930bc
PIKU <160405297+prabeshkhatiwada29@users.noreply.github.com>
2025-06-21 07:33:05 +0545

    Blog api

--- backend/blogapi/blog/admin.py
+++ backend/blogapi/blog/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
+    list_display = ('title', 'created_at', 'author', 'is_published', 'comments_count', 'likes_count')
     search_fields = ('title',)
-    readonly_fields = ('created_at',)
+    list_filter = ('is_published', 'author')
+
+
     
 admin.site.register(Post, PostAdmin)
--- backend/blogapi/blog/models.py
+++ backend/blogapi/blog/models.py
@@ -5,6 +5,11 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    author = models.CharField(max_length=100)
+    tags = models.CharField(max_length=100, blank=True, null=True)
+    comments_count = models.PositiveIntegerField(default=0)
+    likes_count = models.PositiveIntegerField(default=0)
+    is_published = models.BooleanField(default=True)
     
 
     def __str__(self):
